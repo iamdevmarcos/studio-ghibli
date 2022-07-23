3d1336d3a7a6806e7768f6a1f3c545ba938acae4
Marcos Andre <marcosdev.me@gmail.com>
2022-07-23 07:28:42 -0300

    feat: add home template

---
+++ src/templates/Home/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const HomeTemplate = () => (
+  <S.Wrapper>
+    <h1>dad</h1>
+  </S.Wrapper>
+)
+
+export default HomeTemplate
---
+++ src/templates/Home/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background: linear-gradient(130.02deg, #628e75 0.66%, #1a4855 83.32%);
+  min-height: 100vh;
+`
