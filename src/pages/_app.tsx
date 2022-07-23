e7ff4c9170df9e843493a7acb7e6282a338ca362
Marcos Andre <marcosdev.me@gmail.com>
2022-07-23 13:28:30 -0300

    refactor: adjust logo path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,8 @@ function App({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={theme}>
       <Head>
         <title>Studio Ghibli</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="shortcut icon" href="/img/logo.png" />
+        <link rel="apple-touch-icon" href="/img/logo.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
