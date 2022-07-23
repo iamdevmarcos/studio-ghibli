82959b33c8ecfdd18bd46a320cdd9f150e3ae81e
Marcos Andre <marcosdev.me@gmail.com>
2022-07-23 07:17:54 -0300

    chore: add poppins font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,13 +7,20 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Next.js - Boilerplate</title>
+        <title>Studio Ghibli</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components by Marcos Mendes"
+        />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;900&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <GlobalStyles />
